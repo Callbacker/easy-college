1b770252b3991f88615c0947bf82e6bb9117a467
tanweida <acallbacker@gamil.com>
2019-04-24 07:57:56 +0800

    update db config

---
+++ src/main/java/com/weida/easycollege/easycollege/EasyCollegeBoot.java
@@ -0,0 +1,13 @@
+package com.weida.easycollege.easycollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCollegeBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCollegeBoot.class, args);
+    }
+
+}
---
+++ src/test/java/com/weida/easycollege/easycollege/EasyCollegeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weida.easycollege.easycollege;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyCollegeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
