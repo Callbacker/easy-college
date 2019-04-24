4036f682af51252d8700fad5737d702a142367dd
weida <weidatan@cargofe.com>
2019-04-24 13:57:05 +0800

    增加jpa配置

--- src/main/java/com/weida/easycollege/service/AuthorityService.java
+++ src/main/java/com/weida/easycollege/service/AuthorityService.java
@@ -3,9 +3,12 @@ package com.weida.easycollege.service;
 import com.weida.easycollege.dto.IdsDto;
 import com.weida.easycollege.dto.ReturnDto;
 import com.weida.easycollege.interfaces.IAuthority;
+import com.weida.easycollege.po.UserPo;
 import com.weida.easycollege.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthorityService extends BaseService implements IAuthority {
@@ -13,6 +16,10 @@ public class AuthorityService extends BaseService implements IAuthority {
     UserRepository userRepository;
     @Override
     public ReturnDto login(IdsDto idsDto) {
+        Optional<UserPo> optionalUserPo = userRepository.findById(idsDto.getUserId());
+        if (optionalUserPo.isPresent()){
+            return ReturnDto.okData("login: name-"+optionalUserPo.get().getName());
+        }
         return ReturnDto.fail("login:"+idsDto.getUserId());
     }
 }
