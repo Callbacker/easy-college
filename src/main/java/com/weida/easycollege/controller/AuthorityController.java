ce27cc92ad59c981c10062511609a24eac8b28f0
weida <weidatan@cargofe.com>
2019-04-24 12:50:21 +0800

    swagger访问成功

--- src/main/java/com/weida/easycollege/controller/AuthorityController.java
+++ src/main/java/com/weida/easycollege/controller/AuthorityController.java
@@ -6,10 +6,7 @@ import com.weida.easycollege.interfaces.IAuthority;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -18,7 +15,7 @@ public class AuthorityController {
 
     @Autowired
     IAuthority authority;
-    @GetMapping("/login")
+    @PostMapping("/login")
     @ApiOperation(value="登录")
     public ReturnDto login(@RequestBody IdsDto idsDto){
         return authority.login(idsDto);
